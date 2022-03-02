fe6da7c79aa9d079a9bddabb5cf74d612efecef5
drnserhio <duran199756@gmail.com>
2022-03-02 17:09:55 +0200

    create

---
+++ src/main/java/com/sound/labsound/LabSoundApplication.java
@@ -0,0 +1,13 @@
+package com.sound.labsound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabSoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sound/labsound/LabSoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sound.labsound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
